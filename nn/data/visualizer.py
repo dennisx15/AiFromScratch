a1102209f953f86707528ca695995616451030a1
dennisx15 <dennisalacahanlii@gmail.com>
2026-05-02 19:30:18 -0600

    fixed some magic numbers

--- nn/data/visualizer.py
+++ nn/data/visualizer.py
@@ -16,7 +16,7 @@ def plot_training(losses, accuracies):
 
     plt.show()
 
-def plot_confusion_matrix(model, X, y):
+def plot_confusion_matrix(model, X, y, width=8, height=6):
     """
     :param model: to make the predictions
     :param X: the inputs of a dataset
@@ -27,23 +27,24 @@ def plot_confusion_matrix(model, X, y):
     logits = model.forward(X)
     preds = xp.argmax(logits, axis=1)
     cm = confusion_matrix(y, preds)
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(width, height))
     sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
     plt.xlabel("Predicted")
     plt.ylabel("Actual")
     plt.title("Confusion Matrix")
     plt.show()
 
-def show_predictions(model, X, y, n=5):
+def show_predictions(model, X, y, size=28, n=5):
     """
     :param model: to make the predictions
     :param X: the inputs of a dataset
     :param y: the truth labels
     :param n: number of random samples to choose from the dataset
+    :param size: size of the image
     visualize the predictions
     """
     for i in range(n):
-        img = X[i].reshape(28, 28)
+        img = X[i].reshape(size, size)
 
         logits = model.forward(X[i:i+1])
         pred = logits.argmax(axis=1)[0]
@@ -51,3 +52,12 @@ def show_predictions(model, X, y, n=5):
         plt.imshow(img, cmap='gray')
         plt.title(f"Pred: {pred}, True: {y[i]}")
         plt.show()
+
+
+def display_image(image):
+    """
+    Function to display the image
+    :param image: image to display
+    """
+    plt.imshow(image, cmap='gray')
+    plt.show()
