40a2c13b87bf3a0aaac2edc6d5916f5bc2e8f282
dennisx15 <dennisalacahanlii@gmail.com>
2026-05-13 12:00:58 -0600

    Implemented backward pass

--- nn/layers/conv2d.py
+++ nn/layers/conv2d.py
@@ -36,6 +36,9 @@ class Conv2D(Layer):
         Calculate output shape
         :param X: the input, some kind of tensor
         :return: the dimensions of the output
+        Note:
+        Intended for internal Conv2D use only.
+
         """
         batch, channel, height, width = X.shape
 
@@ -56,8 +59,47 @@ class Conv2D(Layer):
             out_w
         )
 
+
+    def get_patch_bounds(self, i, j):
+        """
+        :param i: row index
+        :param j: column index
+        :return: the mapping of the inputs the weight interacted with
+        """
+        h_start = i * self.stride
+        h_end = i  * self.stride + self.kernel_size
+        w_start = j * self.stride
+        w_end = j * self.stride + self.kernel_size
+        return (h_start, h_end, w_start, w_end)
+
+
+    def extract_patch(self, batch, i, j, X):
+        """
+
+        :param patch_bounds: the mapping of the inputs the weight interacted with
+        :param X: The input to apply the extraction to
+        :return: The extracted patch
+        """
+
+        h_start, h_end, \
+            w_start, w_end = \
+            self.get_patch_bounds(i, j)
+
+        return X[
+            batch,
+            :,
+            h_start:h_end,
+            w_start:w_end
+        ]
+
     def forward(self, X):
+        """
+        Forward pass
+        :param X: The input, some kind of tensor
+        :return: The output matrix after forward pass
+        """
         assert X.ndim == 4, "Conv2D expects input shape (B, C, H, W)"
+        self.input = X
         B, OC, out_h, out_w = self.calculate_output_shape(X)
         output_matrix = xp.zeros((B, OC, out_h, out_w))
 
@@ -71,5 +113,94 @@ class Conv2D(Layer):
         return output_matrix
 
     def backward(self, grad_output):
-        ...
+        """
+        Backward pass for Conv2D.
+
+        :param grad_output:
+            Gradient of the loss with respect to
+            this layer's output.
+
+            Shape:
+            (B, OC, out_h, out_w)
+
+        :return:
+            Gradient of the loss with respect
+            to the input.
+
+            Shape:
+            (B, IC, H, W)
+        """
+
+        # -------------------------
+        # Initialize gradients
+        # -------------------------
+
+        self.dW.fill(0)
+        self.db.fill(0)
+
+        dX = xp.zeros_like(self.input)
+
+        # -------------------------
+        # Shapes
+        # -------------------------
+
+        B, OC, out_h, out_w = grad_output.shape
+
+        # -------------------------
+        # Main backward loops
+        # -------------------------
+
+        for batch in range(B):
+
+            for out_c in range(OC):
+
+                for i in range(out_h):
+
+                    for j in range(out_w):
+                        # Current upstream gradient
+                        # dL/dP
+
+                        grad = grad_output[
+                            batch,
+                            out_c,
+                            i,
+                            j
+                        ]
+
+                        # Spatial bounds
+
+                        h_start, h_end, \
+                            w_start, w_end = \
+                            self.get_patch_bounds(i, j)
+
+                        # Input patch used during
+                        # forward pass
+
+                        patch = self.input[
+                            batch,
+                            :,
+                            h_start:h_end,
+                            w_start:w_end
+                        ]
+
+                        # Weight gradients
+                        # dL/dW += dL/dP * dP/dW
+                        # dP/dW = patch
+
+                        self.dW[out_c] += (grad * patch)
+
+                        self.db[out_c] += grad # Bias gradients
+
+                        # Input gradients
+                        # Distribute gradient back
+                        # through the kernel
+
+                        dX[
+                            batch,
+                            :,
+                            h_start:h_end,
+                            w_start:w_end
+                        ] += (grad * self.W[out_c])
+
+        return dX
 
