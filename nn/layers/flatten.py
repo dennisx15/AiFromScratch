724b03791e807f8c24c1844e1dcdb66b0a687f35
dennisx15 <dennisalacahanlii@gmail.com>
2026-05-13 12:01:15 -0600

    Implemented flatten layer

---
+++ nn/layers/flatten.py
@@ -0,0 +1,29 @@
+from .base import Layer
+from nn.backend import xp
+
+
+class Flatten(Layer):
+
+    def forward(self, X):
+        """
+        Flatten input except batch dimension.
+
+        Args:
+            X (xp.ndarray):
+                Shape (B, ...)
+
+        Returns:
+            xp.ndarray:
+                Shape (B, flattened_features)
+        """
+
+        self.input_shape = X.shape
+
+        return X.reshape(X.shape[0], -1)
+
+    def backward(self, grad_output):
+        """
+        Reshape gradient back to original input shape.
+        """
+
+        return grad_output.reshape(self.input_shape)
